7c5d623b7c8591045c72d2ca5497e5e6a7c081cf
kkiyama117 <k.kiyama117@gmail.com>
2018-11-28 19:54:50 +0900

    Add devise controllers

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# omniauth controller for user
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # You should configure your model like this:
+  # devise :omniauthable, omniauth_providers: [:twitter]
+
+  def facebook
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      # this will throw if @user is not activated
+      sign_in_and_redirect @user, event: :authentication
+      if is_navigational_format?
+        set_flash_message(:notice, :success, kind: 'Facebook')
+      end
+    else
+      session['devise.facebook_data'] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
+    end
+  end
+
+  # More info at:
+  # https://github.com/plataformatec/devise#omniauth
+
+  # GET|POST /resource/auth/twitter
+  # def passthru
+  #   super
+  # end
+
+  # GET|POST /users/auth/twitter/callback
+  # def failure
+  #   super
+  # end
+
+  # protected
+
+  # The path used when OmniAuth fails
+  # def after_omniauth_failure_path_for(scope)
+  #   super(scope)
+  # end
+end
