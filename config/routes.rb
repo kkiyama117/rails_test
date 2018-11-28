7732164aaf288bf69701b13528df944819777b0a
kkiyama117 <k.kiyama117@gmail.com>
2018-11-28 18:20:47 +0900

    First index page access success

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
 end
