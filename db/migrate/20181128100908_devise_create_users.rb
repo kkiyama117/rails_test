c50addaacea4874d12f6ef48d82bf5ee5bc2afa8
kkiyama117 <k.kiyama117@gmail.com>
2018-12-02 15:40:07 +0900

    Remove omniauth

--- db/migrate/20181128100908_devise_create_users.rb
+++ db/migrate/20181128100908_devise_create_users.rb
@@ -32,10 +32,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :provider
-      t.string :uid
-      t.string :name
-      t.string :token
+      # t.string :provider
+      # t.string :uid
+      # t.string :name
+      # t.string :token
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,6 @@ ActiveRecord::Schema.define(version: 2018_11_28_100908) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "provider"
-    t.string "uid"
-    t.string "name"
-    t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
