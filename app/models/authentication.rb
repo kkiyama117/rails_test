49d60d952910820915c9426a1ba5c7447c78b2e2
kkiyama117 <k.kiyama117@gmail.com>
2018-12-03 11:15:49 +0900

    Add Auth func

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -7,4 +7,28 @@ class Authentication < ApplicationRecord
   validates_presence_of :user_id, :uid, :provider, :token
   validates_uniqueness_of :uid, scope: :provider
   validates :provider, inclusion: { in: %w[github facebook google] }
+
+  def self.from_omniauth(auth, user)
+    auth = find_or_initialize_by provider: auth.provider, uid: auth.uid
+    auth.token = auth.credentials.token
+
+    user.password = Devise.friendly_token.first(10)
+    transaction do
+      auth.user ||= user || User.create!(get_user_data_from(auth))
+      auth.save! if auth.changed?
+    end
+    auth
+  end
+
+  # Data from auth info
+  def self.get_user_data_from(auth)
+    data = {}
+    if auth.provider == 'facebook'
+      name = auth.extra.raw_info.name
+      email = auth.info.email
+      data = { name: name, email: email }
+    end
+    data.store('password', Devise.friendly_token[0, 10])
+    data
+  end
 end
