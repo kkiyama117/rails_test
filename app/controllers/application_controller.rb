01fcd01390a6c44ec450cdcaadac5d9dddae6981
kkiyama117 <k.kiyama117@gmail.com>
2018-12-05 16:57:57 +0900

    Modify app controller with current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
+# Root app controller
 class ApplicationController < ActionController::Base
+  def current_user
+    UserDecorator.decorate(super) if super.present?
+  end
 end
